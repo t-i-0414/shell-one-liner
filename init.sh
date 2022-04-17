8883a649f64d536d08cde986ec7563daf8b8a0db
t-i-0414 <gutlo.ol0414@gmail.com>
2022-04-17 22:20:19 +0900

    modify init.sh

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 
 /bin/bash -c "docker build -t ubuntu2004 ."
 
-/bin/bash -c "docker run --rm -it --name ubuntu2004 ubuntu2004"
+/bin/bash -c "docker run -it --name ubuntu2004 ubuntu2004"
